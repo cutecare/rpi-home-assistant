691f19b2cb1a64f2bd5e7801204c6c49c158019b
cutecare <33788776+cutecare@users.noreply.github.com>
2017-11-23 00:16:57 +0300

    Update build.sh
    correct gattlib dependencies

--- build.sh
+++ build.sh
@@ -75,6 +75,9 @@ CMD [ "python3", "-m", "homeassistant", "--config", "/config" ]
 RUN pip3 install wheel
 RUN pip3 install homeassistant==$HA_VERSION
 RUN pip3 install aiohttp_cors
+
+# Install gattlib
+RUN ln -s /usr/lib/arm-linux-gnueabihf/libboost_python-py35.so /usr/lib/arm-linux-gnueabihf/libboost_python-py34.so
 RUN pip3 install gattlib
 _EOF_
 
